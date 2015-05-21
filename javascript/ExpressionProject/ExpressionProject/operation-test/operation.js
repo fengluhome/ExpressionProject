82812102c35036430dc9b208fa8586acf1432f8e
fengluhome <fengluhome@163.com>
2015-05-21 21:48:43 +0800

    合并

--- javascript/ExpressionProject/ExpressionProject/operation-test/operation.js
+++ javascript/ExpressionProject/ExpressionProject/operation-test/operation.js
@@ -1,1 +1,14 @@
-﻿
+﻿/// <reference path="../js/esl.js" />
+
+
+define(function (require, exports, module) {
+    var assert = new require("./Assert");
+    var expressionParser = require("/expressionParser/main");
+    assert.AreEqual(3, expressionParser.External("1+2"));
+    assert.AreEqual(6, expressionParser.External("1+2+3="));
+    assert.AreEqual(10, expressionParser.External("1+2+3+4="));
+
+
+
+
+});
